3e5f24fda54e358f44ad9c8e40aa4edd3fa70550
abatima <renat.kulalov.work@gmail.com>
2026-02-07 08:30:40 +0100

    added writing correct answer on the map

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import turtle
 import pandas
 
 screen = turtle.Screen()
+written_state = turtle.Turtle()
 screen.title("US States Game")
 background_image = "blank_states_img.gif"
 correct_counter = 0
+guessed_states = []
 game_is_on = True
 screen.addshape(background_image)
 turtle.shape(background_image)
@@ -12,12 +14,21 @@ turtle.shape(background_image)
 data = pandas.read_csv("50_states.csv")
 
 while game_is_on:
-    guessed_state = screen.textinput(title=f"{correct_counter}/50 Guess the state", prompt="What's the name?").title()
+    answer_state = screen.textinput(title=f"{correct_counter}/50 Guess the state", prompt="What's the name?").title()
 
     for state in data.state:
-        if state == guessed_state:
+        if state == answer_state and answer_state not in guessed_states:
+            guessed_states.append(answer_state)
             correct_counter += 1
 
+            #drawing
 
-    print(guessed_state)
+            state_row = data[data.state == answer_state]
+            written_state.teleport(state_row.x.item(), state_row.y.item())
+            written_state.hideturtle()
+            written_state.write(answer_state)
+
+
+
+    print(answer_state)
 turtle.mainloop()
